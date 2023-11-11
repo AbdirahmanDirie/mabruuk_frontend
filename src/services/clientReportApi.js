20376e3abf0a4d5fd60d812334cb37dea4e19cca
Abdirahman Dirie <cabdirahmandirie@gmail.com>
2023-11-11 09:19:45 +0300

    Update clientReportApi.js

--- src/services/clientReportApi.js
+++ src/services/clientReportApi.js
@@ -4,7 +4,7 @@ const user = JSON.parse(localStorage.getItem('user'));
 
 export const clientReportApi = createApi({
     reducerPath: "clientReportApi",
-    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:8000"}),
+    baseQuery: fetchBaseQuery({baseUrl: "https://mabruuk-api.vercel.app"}),
     tagTypes: ['clientReport'],
     endpoints: (builder) => ({
 
